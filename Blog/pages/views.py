61835f64007547b2520a7f9e8f61a87f02625d1c
Yaosanqi137 <zxc18023571263@outlook.com>
2025-03-30 01:12:41 +0800

    feat: remove the requirement of login to visit main page

--- Blog/pages/views.py
+++ Blog/pages/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-@login_required(login_url='user:login')
 def homepage_view(request):
     return render(request, "Hub.html")
