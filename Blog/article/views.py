5fff157d7bdee2c36e69459525aba81c8b64231d
Yaosanqi137 <zxc18023571263@outlook.com>
2025-03-31 18:00:51 +0800

    feat: update article app and hub

--- Blog/article/views.py
+++ Blog/article/views.py
@@ -1,3 +1,4 @@
+import markdown
 from django.shortcuts import render
 from .models import Article
 
@@ -8,4 +9,8 @@ def list_view(request):
 
 def detail_view(request, id):
     article = Article.objects.get(id=id)
+    article.body = markdown.markdown(article.body, extensions=[
+        'markdown.extensions.extra',
+        'markdown.extensions.codehilite'
+        ])
     return render(request, 'Article.html', {'article': article})
