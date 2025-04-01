8e0ba8e17e8b767c75660a52b0f16851ba8503c2
Yaosanqi137 <zxc18023571263@outlook.com>
2025-04-01 17:40:00 +0800

    feat: add article post and delete function

--- Blog/article/urls.py
+++ Blog/article/urls.py
@@ -6,4 +6,6 @@ app_name = 'article'
 urlpatterns = [
     path('list', views.list_view, name='list'),
     path('detail/<int:id>', views.detail_view, name='detail'),
+    path('edit', views.edit_view, name='edit'),
+    path('delete/<int:id>', views.delete_view, name='delete'),
 ]
