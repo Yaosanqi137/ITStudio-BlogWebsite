d30148cf94e5c15d19cf9fe4b0921d431d9e83bb
LZD18 <2403041037@qq.com>
2025-04-05 22:58:53 +0800

    The function of randomly displaying article thumbnails and user avatars has been added

--- Blog/article/models.py
+++ Blog/article/models.py
@@ -1,3 +1,5 @@
+from os import listdir
+from tkinter import image_names
 from django.db import models
 from user.models import BlogUser
 from django.utils import timezone
@@ -5,6 +7,9 @@ from imagekit.models import ProcessedImageField
 from imagekit.processors import ResizeToFill
 from PIL import Image
 from io import BytesIO
+import os
+import random
+from django.conf import settings
 
 category = {
     "生活": "生活",
@@ -16,10 +21,19 @@ category = {
     "其他": "其他",
 }
 
+def get_random_image():
+    images_directory = os.path.join(settings.MEDIA_ROOT,'articles')
+    images_files = [f for f in os.listdir(images_directory) if f.endswith(('.png','.jpg'))]
+    if images_files:
+        random_image = random.choice(images_files)
+        return os.path.join('articles',random_image)
+    else:
+        return 'articles/default.png'
+
 class Article(models.Model):
     author       = models.ForeignKey(BlogUser, on_delete=models.CASCADE, verbose_name="文章作者")
     title        = models.CharField("标题", max_length=100)
-    head_img     = ProcessedImageField(verbose_name="文章头图", upload_to='articles/%Y/%m/%d', default='articles/default.png', processors=[ResizeToFill(384, 216)])
+    head_img     = ProcessedImageField(verbose_name="文章头图", upload_to='articles/%Y/%m/%d', default=get_random_image(), processors=[ResizeToFill(384, 216)])
     body         = models.TextField(verbose_name="文章正文")
     created_time = models.DateTimeField("发布时间", default=timezone.now)
     updated_time = models.DateTimeField("更新时间", auto_now=True)
--- Blog/user/models.py
+++ Blog/user/models.py
@@ -6,6 +6,8 @@ from imagekit.processors import ResizeToFill
 from PIL import Image
 from io import BytesIO
 import os
+import random
+from django.conf import settings
 
 MEDIA_ADDR = "https://localhost:8000/images/"
 
@@ -15,9 +17,18 @@ sex = {
     "保密": "保密"
 }
 
+def get_random_avatars():
+    images_directory = os.path.join(settings.MEDIA_ROOT,'avatars')
+    images_files = [f for f in os.listdir(images_directory) if f.endswith(('.png','.jpg'))]
+    if images_files:
+        random_image = random.choice(images_files)
+        return os.path.join('avatars',random_image)
+    else:
+        return 'avatars/default.png'
+
 class BlogUser(User, models.Model):
     nickname = models.CharField("昵称", max_length=50, default=f"OUCer_{int(time.time())}", unique=True)
-    avatar   = ProcessedImageField(verbose_name="头像", upload_to='avatars/%Y/%m/%d', default='avatars/default.png', processors=[ResizeToFill(160, 160)])
+    avatar   = ProcessedImageField(verbose_name="头像", upload_to='avatars/%Y/%m/%d', default=get_random_avatars(), processors=[ResizeToFill(160, 160)])
     info     = models.TextField("简介", blank=True, null=True)
     birthday = models.DateField("生日", null=True, blank=True)
     reg_time = models.DateField("注册时间", auto_now_add=True, editable=False)
